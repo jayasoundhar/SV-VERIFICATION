8eae04108fbc5ed9a4e000be4cfce45554e5885b
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-08-08 15:38:08 +0530

    Create driver.sv

---
+++ 02.DFF/driver.sv
@@ -0,0 +1,20 @@
+class driver;
+  transaction trans;
+  mailbox g2d;
+  virtual int_f int_d;
+  
+  function new(virtual int_f int_d,mailbox g2d);
+    this.g2d = g2d;
+    this.int_d = int_d;
+  endfunction
+  
+  task run();
+    trans = new();
+    forever begin
+      g2d.get(trans);
+      int_d.rst = trans.rst;
+      int_d.s = trans.s;
+      trans.display("DRIVER");
+    end
+  endtask
+endclass
