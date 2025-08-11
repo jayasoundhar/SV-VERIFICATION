9e09589f0b51a34092815cf7c2ad4e9c342718d6
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-08-11 15:23:02 +0530

    Add files via upload

---
+++ 02.DFF/generator.sv
@@ -0,0 +1,19 @@
+class generation;
+  transaction trans;
+  mailbox g2d;
+  event st_gn,st_dr;
+  function new(mailbox g2d,event st_gn);
+    this.st_gn = st_gn;
+    this.g2d = g2d;
+  endfunction
+  
+  task run();
+      trans = new();
+    forever begin
+      trans.randomize();
+      g2d.put(trans);
+      trans.display("generator");
+      @st_gn;
+    end
+  endtask
+endclass
