090526dee7c02eec9a7aa18e084fdbe2b9499c9a
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-08-11 10:28:28 +0530

    Create driver.sv

---
+++ 04.S-RAM/driver.sv
@@ -0,0 +1,20 @@
+class driver;
+  transaction trans;
+  mailbox g2d;
+  virtual int_f int_d;
+  function new(virtual int_f int_d,mailbox g2d);
+    this.g2d = g2d;
+    this.int_d = int_d;
+  endfunction
+  
+  task run();
+      trans = new();
+    forever begin
+      g2d.get(trans);
+      int_d.data_in = trans.data_in;
+      int_d.addr = trans.addr;
+      trans.display("driver");
+    end
+  endtask
+  
+endclass
