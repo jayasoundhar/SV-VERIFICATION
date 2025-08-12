efd92bf0d49f96afe93ec141f7f368dc2b5bc473
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-08-12 09:51:16 +0530

    Delete UP-DOWN-COUNTER directory

--- UP-DOWN-COUNTER/interface.sv
+++
@@ -1,4 +0,0 @@
-interface int_f();
-  logic [3:0] out;
-  logic rst,clk,s;
-endinterface
--- UP-DOWN-COUNTER/test.sv
+++
@@ -1,8 +0,0 @@
-`include "environment.sv"
-program test(int_f int_t);
-  environment ev;
-  initial begin
-    ev = new(int_t);
-    ev.run();
-  end
-endprogram
